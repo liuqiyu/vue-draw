3d0f52fa866ea5dec7e75a36a95054f81ffa269e
jiangmin <1691164691@qq.com>
2019-06-26 11:11:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    // console.log(mxClient)
+  }
+}
+</script>
+
+
